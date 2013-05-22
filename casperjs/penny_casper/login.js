693ef265d9c4d9c02bbb48f830be8fba2e8d9223
xiaodoudoudou8 <liling.hg@gmail.com>
2013-05-22 15:52:02 +0800

    uploadfile test code

--- casperjs/penny_casper/login.js
+++ casperjs/penny_casper/login.js
@@ -10,14 +10,14 @@ var casper = require('casper').create({
     viewportSize: {
         width: 1024,
         height: 768
-    },
+    }
 });
 casper.start(login_url, function(){
 	this.test.assertExists('form#user-login', 'login form exists');
 	this.test.assertExists('h2.element-invisible', 'h2 exists');
 	this.fill('form#user-login',{
 		'name':username,
-		'pass':passwd,
+		'pass':passwd
 	}, true);
 	//this.click('form.contact input[type="submit"]');
 	this.echo('login ........');
@@ -37,26 +37,30 @@ casper.thenOpen('http://touzi101dev1.vcbrands.com/node/add/invcontent', function
 	this.echo('  current url-3: ' + this.getCurrentUrl());
 	this.test.assertExists('form#invcontent-node-form', 'invcontent-node-form form exists');
 	this.test.assertExists('select#edit-field-content-type-und', 'the content type drop down exists');
+	this.test.assertExists('edit-field-image-und-0-upload', 'the image path does exists');
 	this.test.assertExists('title', 'article title');
 	this.echo('Page Title: ' + this.getTitle());
+/*	page = require('webpage').create();
+	page.open('http://touzi101dev1.vcbrands.com/node/add/invcontent',function(){
+		page.uploadFile('input[name="files[field_image_und_0]"]', 'C:/Koala.jpg');
+		page.render('screenshort_imageupload.jpg');
+	});*/
 	this.fill('form#invcontent-node-form',{
 		'field_content_type[und]':334,
-		'title':'auto add short article-aq title - by casper'
+		'title':'auto add shortarticle-aq title - by casper',
+		'files[field_image_und_0]':'C:/Koala.jpg'
 	}, true);
-/*	this.evaluate(function() {
-    return document.querySelector('#edit-title').;
-});*/
+	//this.click('input#edit-submit');
 	var select = this.evaluate(function(){
 		//document.querySelector('#edit-field-content-type-und').value = 4; 
 		current_value = this.document.querySelector('#edit-field-content-type-und').options[document.querySelector('#edit-field-content-type-und').selectedIndex].text;
 		return current_value;
-	//eturn current_value;
 	});
 	//this.echo('return result: ' + select);
 	this.echo('current_value: ' + select);
 //	this.echo('fill title:'+ document.querySelector('#edit-field-content-type-und').options[window.document.querySelector('#edit-field-content-type-und').selectedIndex].text);
 	this.echo('fill title:'+ this.getFormValues('form#invcontent-node-form').title);
-	//this.click('input#edit-submit');
+	
 	console.log("clicked");
 });
 /*casper.thenOpen('http://touzi101admin.vcbrands.com/contributors/53305', function(){
